eb911c10f74e93b75b2004c71e7693b3013c7f2f
Maxime Van den Broeck <mavdbroe@ulb.ac.be>
2026-09-01 20:42:53 +0200

    Add balance des comptes view with debit/credit totals

--- comptabilite/views.py
+++ comptabilite/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.db.models import Sum
+from .models import CompteComptable
 
-# Create your views here.
+
+def balance_comptes(request):
+    comptes = CompteComptable.objects.all()
+    lignes = []
+    total_debit = 0
+    total_credit = 0
+
+    for compte in comptes:
+        debit = compte.ecritures_debit.aggregate(total=Sum('montant'))['total'] or 0
+        credit = compte.ecritures_credit.aggregate(total=Sum('montant'))['total'] or 0
+        solde = debit - credit
+
+        if debit or credit:
+            lignes.append({
+                'compte': compte,
+                'debit': debit,
+                'credit': credit,
+                'solde': solde,
+            })
+            total_debit += debit
+            total_credit += credit
+
+    return render(request, 'comptabilite/balance.html', {
+        'lignes': lignes,
+        'total_debit': total_debit,
+        'total_credit': total_credit,
+        'total_solde': total_debit - total_credit,
+    })
--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from comptabilite.views import balance_comptes
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('comptabilite/balance/', balance_comptes, name='balance_comptes'),
 ]
 
 if settings.DEBUG:
