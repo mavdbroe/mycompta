a89b5956294f569f6a1b5ef7bc240c336a4577fe
Maxime Van den Broeck <mavdbroe@ulb.ac.be>
2026-09-01 21:04:06 +0200

    Add PDF link in admin invoice list

--- factures/admin.py
+++ factures/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin, messages
+from django.utils.html import format_html
+from django.urls import reverse
 from .models import Facture, LigneFacture
 from comptabilite.services import generer_ecritures_facture
 
@@ -8,9 +10,14 @@ class LigneFactureInline(admin.TabularInline):
 
 @admin.register(Facture)
 class FactureAdmin(admin.ModelAdmin):
-    list_display = ('numero', 'client', 'date_emission', 'statut', 'total_ttc')
+    list_display = ('numero', 'client', 'date_emission', 'statut', 'total_ttc', 'voir_pdf')
     inlines = [LigneFactureInline]
     actions = ['action_generer_ecritures']
+
+    def voir_pdf(self, obj):
+        url = reverse('facture_pdf', args=[obj.id])
+        return format_html('<a href="{}" target="_blank">Voir PDF</a>', url)
+    voir_pdf.short_description = 'PDF'
 
     @admin.action(description="Générer les écritures comptables")
     def action_generer_ecritures(self, request, queryset):
